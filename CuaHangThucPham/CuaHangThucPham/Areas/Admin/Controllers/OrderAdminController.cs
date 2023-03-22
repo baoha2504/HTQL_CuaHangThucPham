7635446006c838d3fc48b6b41cc38457dbc6913b
Bao <89384723+baoha2504@users.noreply.github.com>
2023-03-22 15:38:54 +0700

    bao push code

---
+++ CuaHangThucPham/CuaHangThucPham/Areas/Admin/Controllers/OrderAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuaHangThucPham.Areas.Admin.Controllers
+{
+    public class OrderAdminController : Controller
+    {
+        // GET: Admin/OrderAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
